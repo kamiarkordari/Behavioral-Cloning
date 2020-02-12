cb3e2fedccfb89e46789358f66d5715126587e42
Kamiar Kordari <kordari@gmail.com>
2020-02-12 11:54:08 -0800

    clean the code

--- clone.py
+++ clone.py
@@ -1,18 +1,21 @@
+import keras
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import MaxPooling2D
+from keras.models import load_model
 import csv
 import cv2
 import numpy as np
-from sklearn.utils import shuffle
 
 
 lines=[]
 
-#dirname = '../behavorial-cloning-data'
-dirname = '/opt/carnd_p3/data'
+dirname = '/opt/carnd_p3/additional_data'
 
 with open(dirname + '/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     next(reader, None)  # skip the header
-
     for line in reader:
         lines.append(line)
 
@@ -26,8 +29,6 @@ for line in lines:
         filename = tokens[-1]
         local_path = dirname+"/IMG/" + filename
         image = cv2.imread(local_path)
-        #image_half_size = cv2.resize(image, (0,0), fx=0.5, fy=0.5)
-        #images.append(image_half_size)
         images.append(image)
 
     correction = 0.2
@@ -35,6 +36,7 @@ for line in lines:
     measurements.append(measurement)
     measurements.append(measurement+correction)
     measurements.append(measurement-correction)
+
 
 augmented_images = []
 augmented_measurements = []
@@ -50,40 +52,24 @@ for image, measurement in zip(images, measurements):
 x_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
-#x_train, y_train = shuffle(x_train, y_train)
-
-
-import keras
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
-from keras.layers.convolutional import Conv2D
-from keras.layers.pooling import MaxPooling2D
-from keras.models import load_model
-
-print(x_train.shape)
-
+# NVIDIA's end-to-end autonomous vehicle architecture
 model = Sequential()
 model.add(Lambda(lambda x: x / 255 - 0.5, input_shape=(160,320,3)))
-#model.add(Lambda(lambda x: x / 255 - 0.5, input_shape=(80,160,3)))
-# Crop images
 model.add(Cropping2D(cropping=((50,20), (0,0))))
-#model.add(Cropping2D(cropping=((25,10), (0,0))))
 model.add(Conv2D(filters=24, kernel_size=(5, 5), strides=(2,2), activation='relu'))
-#model.add(MaxPooling2D())
 model.add(Conv2D(filters=36, kernel_size=(5, 5), strides=(2,2), activation='relu'))
-#model.add(MaxPooling2D())
 model.add(Conv2D(filters=48, kernel_size=(5, 5), strides=(2,2), activation='relu'))
 model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
 model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
-#model.add(MaxPooling2D())
 model.add(Flatten())
 model.add(Dense(100))
-#model.add(Dropout(0.2))
 model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
 
+#model = load_model('model_00.h5')
+
 model.compile(optimizer='adam', loss='mse')
-model.fit(x_train, y_train, validation_split=0.2, shuffle=True, epochs=5)
+model.fit(x_train, y_train, validation_split=0.2, shuffle=True, epochs=3)
 
 model.save('model.h5')
